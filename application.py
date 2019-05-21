126b488b3408920fc5a2f932a64764082db31a54
Kamoliddin <Kamoliddin7usmonov@gmail.com>
2019-05-22 00:00:11 +0900

    Added database

--- application.py
+++ application.py
@@ -1,35 +1,63 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from form import RegistirationForm, LoginForm
-from flask import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c18caca2c8ecee93eda44c67b08c6ec1'
-app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite://database.sql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
 posts = [
-        {
-            'author': 'Kamoliddin Usmonov',
-            'title': 'Blog post 1',
-            'data_posted': 'May 19, 2019',
-            'content': 'First post content'
-        },
-        {
-            'author': 'Bahodir Usmonov',
-            'title': 'Blog post 1',
-            'data_posted': 'May 30, 2019',
-            'content': 'Seond post content'
-        }
+    {
+        'author': 'Kamoliddin Usmonov',
+        'title': 'Blog post 1',
+        'data_posted': 'May 19, 2019',
+        'content': 'First post content'
+    },
+    {
+        'author': 'Bahodir Usmonov',
+        'title': 'Blog post 1',
+        'data_posted': 'May 30, 2019',
+        'content': 'Seond post content'
+    }
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
@@ -38,6 +66,7 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Resgister', form=form)
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -49,6 +78,7 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
